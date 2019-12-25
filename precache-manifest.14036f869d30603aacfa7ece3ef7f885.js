17cce732ebe3a65a291288a4eabc78b45a83b71e
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-25 20:57:28 -0300

    Updates

---
+++ precache-manifest.14036f869d30603aacfa7ece3ef7f885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab518bb143be469f7f37b62c50d0e6d",
+    "url": "/tateti/index.html"
+  },
+  {
+    "revision": "577d5b6bbe0165491e6f",
+    "url": "/tateti/static/css/main.0cd24933.chunk.css"
+  },
+  {
+    "revision": "8c57d9d4d7bb552b6eb2",
+    "url": "/tateti/static/js/2.98708327.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tateti/static/js/2.98708327.chunk.js.LICENSE"
+  },
+  {
+    "revision": "577d5b6bbe0165491e6f",
+    "url": "/tateti/static/js/main.296a5c0a.chunk.js"
+  },
+  {
+    "revision": "2a2a53663f955ab6b4b7",
+    "url": "/tateti/static/js/runtime-main.27c489b0.js"
+  }
+]);
